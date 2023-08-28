7c777b6c5c20d62538e700ceac4f2885c4f77ae3
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-28 18:41:56 +0530

    FEAT: added script to execute to get the test coverage

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,22 +1,21 @@
-import { render, screen } from "@testing-library/react"
-import Greet from "./Greet"
-
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
 
 /**
  * if we follow TDD pattern of development - steps are :-
  * Greet should render the text hello and if a name is passed into the component, is should render hello followed by the name
-*/
+ */
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-})
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders correctly with a name", () => {
-  render(<Greet name="Bruce"/>);
-  const textElement = screen.getByText(/hello Bruce/i);
-  expect(textElement).toBeInTheDocument();
-})
-
-
+  test("renders correctly with a name", () => {
+    render(<Greet name="Bruce" />);
+    const textElement = screen.getByText(/hello Bruce/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
