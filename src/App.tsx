173312bc66244ff2f93c601881fcee1bfa024d93
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-29 16:42:16 +0530

    FEAT: added the skills component and the respective types and test files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Application } from "./components/Application/Application";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
   return (
     // <AppProviders>
     <div className="App">
       <Application />
-      {/* <Skills skills={["HTML", "CSS"]} />
-      <Counter />
+      <Skills skills={["HTML", "CSS", "Javascript"]} />
+      {/* <Counter />
       <CounterTwo count={1} />
       <Users />
     <MuiMode /> */}
