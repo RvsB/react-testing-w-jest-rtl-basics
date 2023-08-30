ec9548134e1d1a5274af422262c3f389a1ad4338
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-30 17:10:08 +0530

    FEAT: added AppProviders containing the theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import "./App.css";
 import { Application } from "./components/Application/Application";
 import { Skills } from "./components/Skills/Skills";
 import { Counter } from "./components/Counter/Counter";
+import { AppProviders } from "./providers/AppProviders";
+import { MuiMode } from "./components/MUI/MuiMode";
 
 function App() {
   return (
-    // <AppProviders>
-    <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS", "Javascript"]} />
-      <Counter />
-      {/* <CounterTwo count={1} />
-      <Users />
-    <MuiMode /> */}
-    </div>
-    //</AppProviders>
+    <AppProviders>
+      <div className="App">
+        <Application />
+        <Skills skills={["HTML", "CSS", "Javascript"]} />
+        <Counter />
+        {/* <CounterTwo count={1} /> */}
+        {/* <Users /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
