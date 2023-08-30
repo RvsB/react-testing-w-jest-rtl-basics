b1ba0b936478fe76c9fc91d433aea5abae100cdf
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-30 16:28:27 +0530

    FEAT: added the counter component and implemented the tests for the same (user and keyboard interactions)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Application } from "./components/Application/Application";
 import { Skills } from "./components/Skills/Skills";
+import { Counter } from "./components/Counter/Counter";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
     <div className="App">
       <Application />
       <Skills skills={["HTML", "CSS", "Javascript"]} />
-      {/* <Counter />
-      <CounterTwo count={1} />
+      <Counter />
+      {/* <CounterTwo count={1} />
       <Users />
     <MuiMode /> */}
     </div>
